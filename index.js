af27c3b8c376bdd711001202c9943ecbc8c3435f
abdihamid <ibrahimabdy58@gmail.com>
2022-11-24 19:40:53 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,70 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(distance){
+    if (distance===43){
+      return 1;
+    }
+    else if(distance===50){
+      return 8
+    }
+    else if(distance<=42){
+      return 8;
+    }
+  }
+  function distanceFromHqInFeet(distance){
+      if(distance==43){
+          return 264;
+      }
+      else if(distance===50){
+          return 2112;
+      }
+      else if(distance<42){
+          return 2112;
+      }
+  
+  }
+  function distanceTravelledInFeet(distance){
+      if((distance>=43)&&(distance<=48)){
+          return 1320;
+      }
+      else if((distance>=50)&&(distance<=60)){
+          return 2640;
+      }
+      else if (distance<42){
+          return 1584;
+      }
+  }
+  function distanceTravelledInFeet(a, b) {
+    if (b > a) {
+      return (b - a) * 264;
+    } else return (a - b) * 264;
+  }
+  function calculatesFarePrice(start, destination){
+    let distance=distanceTravelledInFeet(start,destination)
+     if(distance<=400){
+      return  0;
+     }
+     if(distance>=400 && distance<=2000){
+      return (distance-400)*0.02;
+     }
+     else if(distance>=2000 &&distance<=2500){
+       return 25;
+     }
+     else if(distance>2500){
+      return "cannot travel that far"
+     }
+  }
+function calculatesFarePrice(start, destination){
+    let distance=distanceTravelledInFeet(start, destination)
+    if(distance<=400){
+        return 0;
+    }
+    if(distance>=400 && distance<=2000){
+        return (distance-400)*0.02;
+    }
+    else if(distance>=2000 && distance<=2500){
+        return 25;
+    }
+    else if(distance>2500){
+        return "cannot travel that far"
+    }
+}
